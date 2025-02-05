02b5faccc9d0c7797cd264dc623c61f1f1fe100b
minju <crh0522@naver.com>
2025-02-06 01:04:14 +0900

    add solution 0205

---
+++ 한민주/20250205/P11399.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class P11399 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] waitTimeString = br.readLine().split(" ");
+        int[] waitTimes = new int[n];
+        // 정렬
+        for (int i = 0; i < n; i++) {
+            waitTimes[i] = Integer.parseInt(waitTimeString[i]);
+        }
+
+        Arrays.sort(waitTimes); // 1 2 3 3 4
+      
+        // for i in arr[n]
+        int sum = 0;
+        for (int i : waitTimes) { // 5 8 9 6 4
+            sum += i * n--;
+        }
+
+        System.out.println(sum);
+
+    }
+}
