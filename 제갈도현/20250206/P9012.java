9e72a8eab821a8904e74969243140d3224a1a65e
DohyunJegal <80775466+DohyunJegal@users.noreply.github.com>
2025-02-06 21:48:01 +0900

    파일 이름 오류 수정

--- 제갈도현/20250206/P9012.java
+++ 제갈도현/20250206/P9012.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class P9102 {
+public class P9012 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
