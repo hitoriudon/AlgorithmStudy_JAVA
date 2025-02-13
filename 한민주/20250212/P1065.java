db5644f6996550f86febb68ed273cc16826baef0
mjooee <88558256+mjooee@users.noreply.github.com>
2025-02-13 11:02:46 +0900

    add solution 0212
    코드 약간 수정

--- 한민주/20250212/P1065.java
+++ 한민주/20250212/P1065.java
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-public class Main {
-=======
 package com.ssafy;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class P1065 {
->>>>>>> 44d22db (add solution 0212)
-
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -48,8 +40,5 @@ public class P1065 {
 
 	}
 
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 44d22db (add solution 0212)
+
