a120d3ad9dfd110bc9f2648cff4beb8fafb7821f
leesukcheol <90544907+hitoriudon@users.noreply.github.com>
2025-02-18 15:21:33 +0900

    clear merge conflict
    * add solution 0210
    
    * add solution 0210
    
    * add solution 0210
    
    * add solution 0210
    
    * add solution 0213
    
    * add solution 0213
    
    * add solution 0214

--- 이석철/20250210/P2504.java
+++ 이석철/20250210/P2504.java
@@ -55,49 +55,4 @@ public class P2504 {
             System.out.println(result);
         }
     }
-
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-
-            // (( (() []) () )) = 2* 2* (2* (2 + 3) + 2)
-            // *2 *2 *2 *2 /2, +2 *3 /3, +3 을 생각하기!
-
-            if (c == '(') {
-                stack.push(c);
-                temp *= 2;
-                valueStack.push(temp);
-            } else if (c == '[') {
-                stack.push(c);
-                temp *= 3;
-                valueStack.push(temp);
-            } else if (c == ')') {
-                if (stack.isEmpty() || stack.peek() != '(') {
-                    System.out.println(0);
-                    return;
-                }
-                if (s.charAt(i - 1) == '(') {
-                    result += temp;
-                }
-                stack.pop();
-                temp /= 2;
-            } else if (c == ']') {
-                if (stack.isEmpty() || stack.peek() != '[') {
-                    System.out.println(0);
-                    return;
-                }
-                if (s.charAt(i - 1) == '[') {
-                    result += temp;
-                }
-                stack.pop();
-                temp /= 3;
-            }
-        }
-
-        if (!stack.isEmpty()) {
-            System.out.println(0);
-        } else {
-            System.out.println(result);
-        }
-    }
-
 }
