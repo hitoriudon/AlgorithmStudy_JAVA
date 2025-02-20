d7eca3fe46203ac8bfc96f12bac325fbcb035834
leesukcheol <tjrcjf9@gmail.com>
2025-02-20 09:07:40 +0900

    modify className

--- 이석철/20250205/P1181.java
+++ 이석철/20250205/P1181.java
@@ -1,15 +1,15 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class P1181 {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        
+
         List<String> words = new ArrayList<>();
-        for (int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             words.add(br.readLine());
         }
 
@@ -19,14 +19,14 @@ public class Main {
 
         // 정렬, Comparator 인자 부분을 람다식으로 구현
         Collections.sort(wordsAfterRemoveDuplicate, (o1, o2) -> {
-            if (o1.length() != o2.length()){
+            if (o1.length() != o2.length()) {
                 return Integer.compare(o1.length(), o2.length());
             }
             return o1.compareTo(o2);
         });
-        
+
         System.out.println(wordsAfterRemoveDuplicate);
-        for (String word: wordsAfterRemoveDuplicate){
+        for (String word : wordsAfterRemoveDuplicate) {
             System.out.println(word);
         }
     }
