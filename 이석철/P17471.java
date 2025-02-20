62d4c3d4e63848b482fe4241a37df1b22f3fa942
hitoriudon <tjrcjf9@gmail.com>
2025-02-20 11:43:39 +0900

    update README

--- 이석철/P17471.java
+++ 이석철/P17471.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.*;
 
+/** 게리맨더링 */
 public class P17471 {
     static int n;
     static int[] people;
@@ -11,18 +12,18 @@ public class P17471 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         people = new int[n + 1];
-        
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= n; i++) {
             people[i] = Integer.parseInt(st.nextToken());
         }
-        
+
         nodes = new ArrayList[n + 1];
         for (int i = 0; i <= n; i++) {
-            nodes[i] = new ArrayList<>(); 
+            nodes[i] = new ArrayList<>();
         }
 
-        // 인덱싱 맞춰주기 위해 인덱스 0에 0
+        // 인덱싱 맞춰주기 위해 인덱스 0에 0!
         for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
             int count = Integer.parseInt(st.nextToken());
@@ -30,7 +31,7 @@ public class P17471 {
                 nodes[i].add(Integer.parseInt(st.nextToken()));
             }
         }
-        
+
         makeCombination(1, new ArrayList<>());
         System.out.println(ans == Integer.MAX_VALUE ? -1 : ans);
     }
@@ -40,7 +41,7 @@ public class P17471 {
         if (!voter1.isEmpty()) {
             f(new ArrayList<>(voter1));
         }
-        
+
         for (int i = start; i <= n; i++) {
             voter1.add(i);
             makeCombination(i + 1, voter1);
@@ -56,7 +57,7 @@ public class P17471 {
                 voter2.add(i);
             }
         }
-        
+
         // bfs 함수 ret 값은 boolean 타입
         if (bfs(voter1) && bfs(voter2)) {
             int population1 = 0, population2 = 0;
@@ -72,15 +73,15 @@ public class P17471 {
 
     // 연결이 되어 있으면 true, 안 되어 있으면 false 리턴
     static boolean bfs(List<Integer> voter) {
-        if (voter.isEmpty()) 
+        if (voter.isEmpty())
             return false;
-        
+
         Queue<Integer> que = new ArrayDeque<>();
         boolean[] visited = new boolean[n + 1]; // 0은 항상 비우기
         que.add(voter.get(0));
         visited[voter.get(0)] = true;
         int count = 1;
-        
+
         while (!que.isEmpty()) {
             int node = que.poll();
             for (int neighbor : nodes[node]) {
@@ -91,7 +92,7 @@ public class P17471 {
                 }
             }
         }
-        
+
         return count == voter.size(); // 같으면 연결 된 것
     }
 }
