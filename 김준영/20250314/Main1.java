e44867ede97af6cf218f7b1b064dae602fec7f14
= <=>
2025-03-20 16:23:37 +0900

    add sol 0314

--- 김준영/20250314/Main1.java
+++ 김준영/20250314/Main1.java
@@ -1,35 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
-
-// [1463 1로 만들기]
-// 1. X가 3으로 나누어 떨어지면, 3으로 나눈다.
-// 2. X가 2로 나누어 떨어지면, 2로 나눈다.
-// 3. 1을 뺀다.
-// 목표 : 1로 만들기
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
 		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n + 1];
+		st = new StringTokenizer(br.readLine());
 
-		int[] dp = new int[n + 1];
-
-		dp[1] = 0;
-
-		for (int i = 2; i <= n; i++) {
-			dp[i] = dp[i - 1] + 1;
-
-			if (i % 2 == 0)
-				dp[i] = Math.min(dp[i], dp[i / 2] + 1);
-			if (i % 3 == 0)
-				dp[i] = Math.min(dp[i], dp[i / 3] + 1);
-
+		for(int i = 1; i <= n; i++) {
+			arr[i] += Integer.parseInt(st.nextToken()) + arr[i-1];
 		}
-
-		System.out.println(dp[n]);
-
+		
+		int m = Integer.parseInt(br.readLine());
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			System.out.println(-arr[Integer.parseInt(st.nextToken())- 1] + arr[Integer.parseInt(st.nextToken())]);
+		}
 	}
 }
