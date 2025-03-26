6f3bb59a9bb1a19595afc72ce7b7bfb1e5361b4e
minju <cash0522@hanyang.ac.kr>
2025-03-26 21:31:35 +0900

    add solution 0321

---
+++ 한민주/20250321/P2056.java
@@ -0,0 +1,54 @@
+package algorithm;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class P2056 {
+
+	public static void main(String[] args) throws Exception {
+		// 초기화
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		ArrayList<ArrayList<Integer>> works = new ArrayList<ArrayList<Integer>>(); // 선행 작업 기록
+		int n = Integer.parseInt(br.readLine());
+		int time[] = new int[n + 1]; // 작업 당 걸리는 시간
+		int preWork[] = new int[n + 1]; // 선행 작업 개수
+		StringTokenizer st;
+		ArrayDeque<Integer> todo = new ArrayDeque<Integer>(); // 선행 작업 없는 일
+		for (int i = 0; i <= n; i++) {
+			works.add(new ArrayList<Integer>());
+		}
+
+		// 작업 입력
+		for (int i = 1; i <= n; i++) {
+			st = new StringTokenizer(br.readLine());
+			time[i] = Integer.parseInt(st.nextToken());
+			preWork[i] = Integer.parseInt(st.nextToken());
+
+			// 선행 작업 없으면 todo에 추가
+			if (preWork[i] == 0)
+				todo.add(i);
+
+			// 선행 작업의 후행 작업에 추가
+			for (int j = 0; j < preWork[i]; j++) {
+				int p = Integer.parseInt(st.nextToken());
+				works.get(i).add(p);
+			}
+		}
+
+		// 선행 작업 중 가장 오래 걸리는거 시간에 더하기 => 최댓값
+		int maxT = 0;
+		for (int i = 1; i <= n; i++) {
+			int maxPre = 0;
+			for (int j = 0; j < works.get(i).size(); j++) {
+				maxPre = Math.max(maxPre, time[works.get(i).get(j)]);
+			}
+			time[i] += maxPre;
+			maxT = Math.max(maxT, time[i]);
+		}
+
+		System.out.println(maxT);
+
+	}
+
+}
