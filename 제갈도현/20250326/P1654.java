1bb19b31d3ffde1cafffe9a95779279cb29abf0b
DohyunJegal <80775466+DohyunJegal@users.noreply.github.com>
2025-03-27 19:41:07 +0900

    Update P1654

--- 제갈도현/20250326/P1654.java
+++ 제갈도현/20250326/P1654.java
@@ -1,12 +1,10 @@
 // 메모리 15424kb, 시간 120ms
-package silver;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class s2_1654 {
+public class P1654 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
