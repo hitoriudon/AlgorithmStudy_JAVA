1f5368fa632416c6e4083a727edb4603b1ad2025
DohyunJegal <80775466+DohyunJegal@users.noreply.github.com>
2025-03-27 19:41:39 +0900

    Update P2805

--- 제갈도현/20250327/P2805.java
+++ 제갈도현/20250327/P2805.java
@@ -1,11 +1,10 @@
-package test;
-
+// 메모리 168048kb, 시간 460ms
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class s2_2805 {
+public class P2805 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
